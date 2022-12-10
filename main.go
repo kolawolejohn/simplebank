37d912f1d6937d3c9ebf38975f02417db5ac30ae
kolawolejohn <kolawole.oajohn@gmail.com>
2022-12-10 11:10:05 +0100

    add: add initial implementation

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/kolawolejohn/simplebank/api"
+	db "github.com/kolawolejohn/simplebank/db/sqlc"
+	"github.com/kolawolejohn/simplebank/util"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db", err)
+	}
+
+	store := db.NewStore(conn)
+	server, err := api.NewServer(config, store)
+
+	if err != nil {
+		log.Fatal("Cannot create server:", err)
+	}
+
+	err = server.Start(config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("Cannot start server:", err)
+	}
+
+}
